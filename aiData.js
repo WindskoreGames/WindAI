b47f8428436a9ba891d489ad22d6f00bfed53c4c
WindskoreGames <windskoresweden@outlook.com>
2026-02-05 20:12:39 +0100

    Create aiData.js

---
+++ aiData.js
@@ -0,0 +1,8 @@
+// Objekt med nyckelord och svar
+const aiResponses = {
+    "hej": "Hej på dig!",
+    "tja": "Tja! Hur är läget?",
+    "hur mår du": "Jag mår bra, tack! Och du?",
+    "vad heter du": "Jag har inget namn än, vill du ge mig ett?",
+    "sluta": "Hejdå! Vi ses nästa gång."
+};
